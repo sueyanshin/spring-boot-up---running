479f621a3f4f3b1fc319f074f451fd0b782d5d44
Sue Yan Shin <66193892+sueyanshin@users.noreply.github.com>
2025-12-04 18:55:49 +0630

    first commit

---
+++ start-rest-demo/src/main/java/com/example/start_rest_demo/StartRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.start_rest_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartRestDemoApplication.class, args);
+	}
+
+}
---
+++ start-rest-demo/src/test/java/com/example/start_rest_demo/StartRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.start_rest_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
